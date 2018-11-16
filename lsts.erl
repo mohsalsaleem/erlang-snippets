bb7ff2416186ec55343edf0ec2ee2ba95a87adae
Mohamed Saleem <mohsal.saleem@gmail.com>
2018-11-16 18:59:28 +0530

    Added list length calculation

--- lsts.erl
+++ lsts.erl
@@ -1,8 +1,13 @@
 -module(lsts).
--export([get_fuel_prices/0]).
+-export([get_fuel_prices/0, list_length/1]).
 
 get_fuel_prices() ->
 	[ { chennai, 80.3 }, { delhi, 85.3 }, { mumbai, 82.09 } ].
+
+list_length([]) -> 
+	0;
+list_length([ First | Rest ]) ->
+	1 + list_length(Rest).
 
 % .c(lsts).
 % lsts:get_fuel_prices().
@@ -11,3 +16,4 @@ get_fuel_prices() ->
 % S.
 % T.
 % New.
+% lsts:list_length(lsts:get_fuel_prices()).
