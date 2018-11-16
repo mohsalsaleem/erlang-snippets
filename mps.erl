322700fe30fffdc52ca4f05efadaed055640e58b
Mohamed Saleem <mohsal.saleem@gmail.com>
2018-11-16 19:19:05 +0530

    Added maps

--- mps.erl
+++ mps.erl
@@ -1,7 +1,11 @@
 -module(mps).
--export([new_prices/2]).
+-export([new_prices/2, get_banglore_price/1]).
 
 -define(is_valid(V), (is_float(V) andalso V =< 100.0 andalso V >= 50.0)).
 
 new_prices(Chennai, Banglore) when ?is_valid(Chennai), ?is_valid(Banglore) ->
 	#{ chennai => Chennai, banglore => Banglore }.
+
+get_banglore_price(#{ banglore := Price}) -> 
+	Price.
+	
